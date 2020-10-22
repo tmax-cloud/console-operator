a624b2da967ab87a5b5d37a08e1dd3bce44bb3f5
jinsoo-youn <jinsoo_youn@tmax.co.kr>
2020-10-22 15:10:59 +0900

    [feat] upgrade operator version v1.1

--- pkg/apis/console/group.go
+++
@@ -1,6 +0,0 @@
-// Package console contains console API versions.
-//
-// This file ensures Go source parsers acknowledge the console package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package console
